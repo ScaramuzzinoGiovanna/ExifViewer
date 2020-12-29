8814163f575bf4c69bebc86a3b28001d64d90d48
Giovanna Scaramuzzino <scaramuzzinogiovanna95@gmail.com>
2020-12-29 12:29:51 +0100

    add method upload_imgs

--- model.py
+++ model.py
@@ -3,22 +3,23 @@ from observable import Observable
 
 class Model:
     def __init__(self):
-        self.listPreviewImages = Observable('')
+        self.listPreviewImages = Observable()
 
-    def observe(self, slot1, slot2):
-        self.listPreviewImages.observe(slot1, slot2)
+    def observe(self, slot1, slot2, slot3):
+        self.listPreviewImages.observe(slot1, slot2, slot3)
 
     def add_img(self, path_img):
         path_img = path_img[0]
         self.listPreviewImages.values = path_img  # set
 
     def delete_AllImgs(self):
-        # togliere imm visualizzata
         self.listPreviewImages.remove_all_imgs()
 
     def delete_SelectedImgs(self, items):
-        # se è una delle imm che è visuallizata grande, allora devi toglierla pure da li
         self.listPreviewImages.remove_imgs(items)
+
+    def upload_img(self, item):
+        self.listPreviewImages.upload_img(item)
 
 
 M = Model()
