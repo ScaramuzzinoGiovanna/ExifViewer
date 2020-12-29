2d1495b7400bcff1c7dab11bd72016d24a098ce5
Giovanna Scaramuzzino <scaramuzzinogiovanna95@gmail.com>
2020-12-29 12:32:56 +0100

    list preview images - double click event and right button press

---
+++ listPreviewImages.py
@@ -0,0 +1,20 @@
+from PyQt5.QtWidgets import QListWidget
+from PyQt5.QtCore import Qt, QEvent
+
+
+class ListPreviewImages(QListWidget):
+
+    def __init__(self, widget):
+        QListWidget.__init__(self, widget)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.RightButton:
+            self.clearSelection()
+        else:
+            super(ListPreviewImages, self).mousePressEvent(event)
+
+    def mouseDoubleClickEvent(self, event):
+        item = self.currentItem()
+        self.clearSelection()
+        item.setSelected(True)
+        super(ListPreviewImages, self).mouseDoubleClickEvent(event)
