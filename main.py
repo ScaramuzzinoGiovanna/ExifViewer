03926e89f47c02137ef2c6b2b2344e057871859f
Giovanna Scaramuzzino <scaramuzzinogiovanna95@gmail.com>
2020-12-29 12:30:54 +0100

    improved actions in the list

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QLabel, QListWidgetItem
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QLabel, QListWidgetItem, QListView
+from PyQt5 import QtCore
 from PyQt5.QtGui import QPixmap, QIcon
 from ui_exifViewer import Ui_ExifViewer
 from model import M
 import sys
-import os
 from os.path import expanduser
-
 
 
 class ExifViewer(QMainWindow):
@@ -17,17 +15,17 @@ class ExifViewer(QMainWindow):
         self.ui = Ui_ExifViewer()
         self.ui.setupUi(self)
 
-
         self.ui.button_rotateRight.clicked.connect(lambda: print('rotR'))
         self.ui.button_rotateLeft.clicked.connect(lambda: print(''))
         self.ui.button_previous.clicked.connect(lambda: print(''))
         self.ui.button_next.clicked.connect(lambda: print(''))
-        self.ui.button_addImages.clicked.connect(lambda img: M.add_img(
+        self.ui.button_addImages.clicked.connect(lambda: M.add_img(
             QFileDialog.getOpenFileNames(self, 'Open file', expanduser("~"), "Image files (*.jpg)")))
-        self.ui.button_deleteAllImages.clicked.connect(lambda: M.delete_AllImgs())
+        self.ui.button_deleteAllImages.clicked.connect(M.delete_AllImgs)
         self.ui.button_deleteSelectedImages.clicked.connect(
             lambda: M.delete_SelectedImgs(self.ui.listWidget.selectedItems()))
-        M.observe(self.show_preview_image_into_list, self.delete_preview)
+        self.ui.listWidget.itemDoubleClicked.connect(lambda: M.upload_img(self.ui.listWidget.currentItem()))
+        M.observe(self.show_preview_image_into_list, self.delete_preview, self.show_img)
 
     def show_preview_image_into_list(self, img_path):
         for img in img_path:
@@ -36,12 +34,23 @@ class ExifViewer(QMainWindow):
             item.setToolTip(img)
             self.ui.listWidget.addItem(item)
 
-    def delete_preview(self, items):
+    def delete_preview(self, sign):
+        items = sign[0]
+        delCurrentImageUp = sign[1]
         if items == 1:  # flag che indica che devo eliminare tutte le immagini
             self.ui.listWidget.clear()
         else:
             for item in items:
                 self.ui.listWidget.takeItem(self.ui.listWidget.row(item))
+        if delCurrentImageUp == True:
+            self.ui.image.clear()
+            self.defaultImage()
+
+    def defaultImage(self):
+        self.ui.image.setText('Select image')
+
+    def show_img(self, path_img):
+        self.ui.image.upload_img(path_img)
 
 
 if __name__ == '__main__':
